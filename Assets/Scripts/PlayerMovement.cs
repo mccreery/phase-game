0f75781d98f121009052286a521f9271b52f8dd1
Leo <leomoreemail@gmail.com>
2020-02-16 22:38:30 +0000

    Wall Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float moveSpeed = 5f;
+	public float moveSpeed = 7f;
 	public float jumpForce = 5f;
-	public bool isGrounded = true;
+	public float wallJumpForce = 7f;
 
 	WallTest wallTest;
 
@@ -20,25 +20,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 		moveHorizontal();
-
 		Jump();
     }
-
+		
 	// Move horizontal
 	void moveHorizontal() 
 	{
 		float horizontalInput = Input.GetAxis("Horizontal");
 		Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
 
-		if (wallTest.Test(WallType.Horizontal)) {
-			if (wallTest.Test(WallType.LeftWall) && horizontalInput > 0) {
-				transform.position += movement * Time.deltaTime * moveSpeed;
-			}
-			else if (wallTest.Test(WallType.RightWall) && horizontalInput < 0) {
-				transform.position += movement * Time.deltaTime * moveSpeed;
-			}
-
-		} else {
+		if (wallTest.Test(WallType.Floor) && wallTest.Test(WallType.LeftWall) && horizontalInput > 0) {
+			transform.position += movement * Time.deltaTime * moveSpeed;
+		}
+		else if (wallTest.Test(WallType.Floor) && wallTest.Test(WallType.RightWall) && horizontalInput < 0) {
+			transform.position += movement * Time.deltaTime * moveSpeed;
+		}
+		else if (!wallTest.Test(WallType.Horizontal)) {
 			transform.position += movement * Time.deltaTime * moveSpeed;
 		}
 	}
@@ -46,8 +43,14 @@ public class PlayerMovement : MonoBehaviour
 	// Jump
 	void Jump()
 	{
-		if (Input.GetButtonDown("Jump")  && wallTest.Test(WallType.Floor)) {
-			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+		if (Input.GetButtonDown("Jump")) {
+			if (wallTest.Test(WallType.Floor)) {
+				gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+			} else if (wallTest.Test(WallType.LeftWall)) {
+				gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(wallJumpForce, jumpForce), ForceMode2D.Impulse);
+			} else if (wallTest.Test(WallType.RightWall)) {
+				gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-wallJumpForce, jumpForce), ForceMode2D.Impulse);
+			} 
 		}
 	}
 }
