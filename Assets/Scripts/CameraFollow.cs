51c5268ba54e43b535f1a0268f07536294accaa6
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-17 21:44:29 +0000

    Limit the camera to the borders of the level

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,24 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
-	private Vector3 velocity = Vector3.zero;
+	private Vector2 velocity;
 	public Transform target;
 	public float dampTime = 0.25f;
 
-    // Start is called before the first frame update
-    void Start()
+    private new Camera camera;
+    public Collider2D limits;
+
+    private void Start()
     {
-		
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-		Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.4f, point.z));
-		Vector3 destination = transform.position + delta;
-		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		Vector2 delta = target.position - camera.ViewportToWorldPoint(new Vector2(0.5f, 0.4f));
+		Vector2 destination = (Vector2)transform.position + delta;
+        Vector2 position = Vector2.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+        Vector2 cameraExtents = new Vector2(
+            camera.orthographicSize * Screen.width / Screen.height,
+            camera.orthographicSize);
+
+        Bounds bounds = limits.bounds;
+        bounds.extents -= (Vector3)cameraExtents;
+
+        position.x = Mathf.Clamp(position.x, bounds.min.x, bounds.max.x);
+        position.y = Mathf.Clamp(position.y, bounds.min.y, bounds.max.y);
+        transform.position = position;
     }
 }
