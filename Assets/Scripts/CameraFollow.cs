a913cd3352df8ff958a05b2e7debb7e527fc62e5
Leo <leomoreemail@gmail.com>
2020-02-17 15:08:19 +0000

    Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public float dampTime = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+		Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.4f, point.z));
+		Vector3 destination = transform.position + delta;
+		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+    }
+}
