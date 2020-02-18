9c02684cd459c92334430b9ac261df0684dfc259
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-18 17:01:22 +0000

    Make camera snap to correct position at scene start

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -20,10 +20,12 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Vector2 delta = target.position - camera.ViewportToWorldPoint(new Vector2(0.5f, 0.4f));
-		Vector2 destination = (Vector2)transform.position + delta;
-        Vector2 position = Vector2.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        transform.position = Vector2.SmoothDamp(transform.position, target.position, ref velocity, dampTime);
+        Clamp();
+    }
 
+    private void Clamp()
+    {
         Vector2 cameraExtents = new Vector2(
             camera.orthographicSize * Screen.width / Screen.height,
             camera.orthographicSize);
@@ -31,8 +33,8 @@ public class CameraFollow : MonoBehaviour
         Bounds bounds = limits.bounds;
         bounds.extents -= (Vector3)cameraExtents;
 
-        position.x = Mathf.Clamp(position.x, bounds.min.x, bounds.max.x);
-        position.y = Mathf.Clamp(position.y, bounds.min.y, bounds.max.y);
-        transform.position = position;
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, bounds.min.x, bounds.max.x),
+            Mathf.Clamp(transform.position.y, bounds.min.y, bounds.max.y));
     }
 }
