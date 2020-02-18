15c24996b64613e72363b6df37135626ce4e687e
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-18 17:02:46 +0000

    Add reset button (key R)

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject player;
+    public Vector2 resetPosition;
+
+    public EnergyMeter meter;
+    public new Camera camera;
+
+    private void Start()
+    {
+        DoReset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Reset"))
+        {
+            DoReset();
+        }
+    }
+
+    private void DoReset()
+    {
+        player.transform.position = resetPosition;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        meter.Energy = meter.startEnergy;
+        camera.transform.position = player.transform.position;
+    }
+}
