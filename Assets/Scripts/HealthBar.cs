4da2c97838ae5016e6cf4632965b6fd1df0cd770
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-25 18:18:24 +0000

    Add health bar to HUD

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject heartImage;
+
+    public int maxHealth;
+    private int health;
+
+    public int Health
+    {
+        get => health;
+        set
+        {
+            health = value;
+
+            int i = 0;
+            foreach (Transform childTransform in transform)
+            {
+                GameObject child = childTransform.gameObject;
+                child.SetActive(i < health);
+                ++i;
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        Health = maxHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < maxHealth; i++)
+        {
+            Instantiate(heartImage, transform);
+        }
+        Reset();
+    }
+}
