0b99b7b61449c80e57e118d7f6c6d13bfe931c96
Leo <leomoreemail@gmail.com>
2020-02-27 19:59:38 +0000

    Update menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,15 +7,29 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button play;
+    public Button options;
+    public Button quit;
 
     void Start()
     {
         play.onClick.AddListener(Play);
+        options.onClick.AddListener(Options);
+        quit.onClick.AddListener(Quit);
     }
 
     void Play()
     {
-        //SceneManager.LoadScene("Prototype");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    void Options()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+
 }
