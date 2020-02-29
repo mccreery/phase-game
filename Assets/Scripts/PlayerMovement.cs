a99b2a513ada265765b51524f03255238c57d3a0
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-29 18:20:50 +0000

    Add animator for player sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(EnergyMeter))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
     public float runAcceleration = 1.0f;
@@ -17,12 +19,16 @@ public class PlayerMovement : MonoBehaviour
     private bool jumpPending;
 
     private EnergyMeter meter;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         collider2D = GetComponent<Collider2D>();
         meter = GetComponent<EnergyMeter>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -36,7 +42,16 @@ public class PlayerMovement : MonoBehaviour
         wallsHit = WallTest.GetFlags(collider2D);
 
         Vector2 velocity = rigidbody2D.velocity;
-        velocity.x = Mathf.MoveTowards(velocity.x, runMaxVelocity * Input.GetAxisRaw("Horizontal"), runAcceleration * Time.deltaTime);
+        float inputX = Input.GetAxisRaw("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, runMaxVelocity * inputX, runAcceleration * Time.deltaTime);
+
+        animator.SetFloat("GroundSpeed", Mathf.Abs(velocity.x));
+        animator.SetBool("OnFloor", wallsHit.Any(WallFlags.Floor));
+        animator.SetBool("OnWall", wallsHit.Any(WallFlags.Horizontal));
+        if (inputX != 0)
+        {
+            spriteRenderer.flipX = inputX < 0;
+        }
 
         // Player still has to be pressing and touching a wall for the jump to succeed
         if (jumpPending && Input.GetButton("Jump") && wallsHit.Any(WallFlags.Floor | WallFlags.Horizontal))
