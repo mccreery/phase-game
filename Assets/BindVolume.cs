6771d39851045bf1d26d837141e267da4b19a32a
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-03 22:56:30 +0000

    Link volume options to menu and persist sliders

--- Assets/BindVolume.cs
+++ Assets/BindVolume.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class BindVolume : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider slider;
     public string parameterName;
+
+    private void Start()
+    {
+        float dB;
+        audioMixer.GetFloat(parameterName, out dB);
+        slider.value = DBToVolume(dB);
+    }
 
     public void SetVolume(float value)
     {
-        audioMixer.SetFloat(parameterName, VolumeTo_dB(value));
+        audioMixer.SetFloat(parameterName, VolumeToDB(value));
     }
 
-    public static float VolumeTo_dB(float volume)
+    public static float VolumeToDB(float volume)
     {
         return Mathf.Max(-80.0f, 20.0f * Mathf.Log10(volume));
+    }
+
+    public static float DBToVolume(float dB)
+    {
+        return Mathf.Pow(10, dB / 20.0f);
     }
 }
