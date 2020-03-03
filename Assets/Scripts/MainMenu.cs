d4ac78a9775827e5907f2ad5dba2a29de37dddc8
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-03 21:07:49 +0000

    Give main menu consistent buttons and link

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,35 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button play;
-    public Button options;
-    public Button quit;
-
-    void Start()
+    public void LoadScene(Object scene)
     {
-        play.onClick.AddListener(Play);
-        options.onClick.AddListener(Options);
-        quit.onClick.AddListener(Quit);
+        SceneManager.LoadScene(scene.name);
     }
 
-    void Play()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    void Options()
-    {
-        SceneManager.LoadScene("OptionsMenu");
-    }
-
-    void Quit()
+    public void Quit()
     {
         Application.Quit();
     }
-
 }
