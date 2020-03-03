0343f3e0bd56e54fc1a847895b4948c62f8d43ea
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-03 20:27:46 +0000

    Add level select screen with buttons

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject buttonTemplate;
+    public List<Object> scenes = new List<Object>();
+
+    void Start()
+    {
+        foreach (Object scene in scenes)
+        {
+            GameObject buttonObject = Instantiate(buttonTemplate, transform);
+            if (eventSystem.firstSelectedGameObject == null)
+            {
+                eventSystem.firstSelectedGameObject = buttonObject;
+            }
+
+            buttonObject.GetComponentInChildren<Text>().text = scene.name;
+
+            Button button = buttonObject.GetComponent<Button>();
+            button.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(scene.name);
+            });
+        }
+    }
+}
