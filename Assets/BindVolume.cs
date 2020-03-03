d725b22a5bb876be4bb22c3cf188b98c4fd8f72f
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-03 22:48:27 +0000

    Add volume options scene bound to a test mixer

---
+++ Assets/BindVolume.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class BindVolume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public string parameterName;
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat(parameterName, VolumeTo_dB(value));
+    }
+
+    public static float VolumeTo_dB(float volume)
+    {
+        return Mathf.Max(-80.0f, 20.0f * Mathf.Log10(volume));
+    }
+}
