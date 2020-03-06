3caf493a74282528b230892ee8c171b376cd1ad7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-06 20:28:35 +0000

    Add kill splat effect before reload

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -12,12 +12,13 @@ public class HealthManager : MonoBehaviour
         get => health;
         set
         {
-            bool changed = health != value;
+            int oldHealth = health;
             health = value;
-            CheckHealth();
 
-            if (changed)
+            CheckHealth();
+            if (oldHealth != health)
             {
+                SetInvul();
                 healthChanged.Invoke();
             }
         }
@@ -28,12 +29,13 @@ public class HealthManager : MonoBehaviour
     public int MaxHealth => maxHealth;
 
     [SerializeField]
-    private UnityEvent healthChanged;
+    private UnityEvent healthChanged = new UnityEvent();
     public UnityEvent HealthChanged => healthChanged;
 
     private void OnValidate()
     {
         CheckHealth();
+        SetInvul();
         healthChanged.Invoke();
     }
 
@@ -41,7 +43,7 @@ public class HealthManager : MonoBehaviour
     {
         if (health <= 0)
         {
-            Kill();
+            StartCoroutine(Kill());
         }
         else if (health > maxHealth)
         {
@@ -61,13 +63,17 @@ public class HealthManager : MonoBehaviour
 
     public bool Invulnerable => invulTime > Time.timeSinceLevelLoad;
 
+    [SerializeField]
+    private GameObject splatEffect;
+
     private void Start()
     {
-        invulTime = Time.timeSinceLevelLoad + invulTimeOnHit;
+        SetInvul();
     }
 
-    void Update()
+    private void SetInvul()
     {
+        invulTime = Time.timeSinceLevelLoad + invulTimeOnHit;
         if (Invulnerable && !blinking)
         {
             blinking = true;
@@ -83,7 +89,7 @@ public class HealthManager : MonoBehaviour
             spriteToBlink.enabled = !spriteToBlink.enabled;
         }
 
-        spriteToBlink.enabled = true;
+        spriteToBlink.enabled = health > 0;
         blinking = false;
     }
 
@@ -92,12 +98,27 @@ public class HealthManager : MonoBehaviour
         if (other.CompareTag("Enemy") && !Invulnerable)
         {
             --Health;
-            invulTime = Time.timeSinceLevelLoad + invulTimeOnHit;
         }
     }
 
-    public void Kill()
+    public IEnumerator Kill()
     {
+        // Make sure the world knows we have no health
+        // Don't set the property directly as this will cause a loop
+        invulTime = 0;
+        health = 0;
+        healthChanged.Invoke();
+
+        spriteToBlink.enabled = false;
+
+        // Can't instantiate from editor until end of frame
+        yield return new WaitForEndOfFrame();
+        Instantiate(splatEffect, transform);
+
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(3);
+        Time.timeScale = 1;
+
         LevelSelect.ReloadLevel();
     }
 }
