efe540c4c2327a36903a51e35516a759d03e5dc2
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-07 18:30:45 +0000

    Add pause menu (no actual pausing)

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+    }
+}
