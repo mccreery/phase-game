00c7aef2fb648e920e0527a9f941cd468a13e09b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-08 15:57:05 +0000

    Fix door script and sprites to open when used

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject player;
+
+    public SpriteRenderer top, bottom;
+    public Sprite topOpenSprite, bottomOpenSprite;
+
+    private bool touchingPlayer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Set player flag
+        touchingPlayer = touchingPlayer || collision.gameObject.CompareTag("Player");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Clear player flag
+        touchingPlayer = touchingPlayer && !collision.gameObject.CompareTag("Player");
+    }
+
+    private void Update()
+    {
+        if (touchingPlayer && player.GetComponent<PlayerMovement>().exit && Input.GetButtonDown("Interact"))
+        {
+            top.sprite = topOpenSprite;
+            bottom.sprite = bottomOpenSprite;
+            LevelManager.Instance.SkipLevels(1);
+        }
+    }
+}
