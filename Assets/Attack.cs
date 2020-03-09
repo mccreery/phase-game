eb96a04f1df2e1a777271021cee58c2dd646be95
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-09 01:22:31 +0000

    Chaser attacks player with slightly smaller hitbox

---
+++ Assets/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HealthManager healthManager = collision.gameObject.GetComponent<HealthManager>();
+            healthManager.Health--;
+        }
+    }
+}
