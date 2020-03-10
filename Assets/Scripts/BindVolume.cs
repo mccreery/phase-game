606ffca324f36b12b08061eaf36a2a89a36fb863
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-10 01:27:59 +0000

    Fix range of audio sliders -80 to 0 replaced by -50 to 0 Sounds much more natural and the bottom half of the slider does something now

--- Assets/Scripts/BindVolume.cs
+++ Assets/Scripts/BindVolume.cs
@@ -8,16 +8,25 @@ public class BindVolume : MonoBehaviour
     public AudioMixer audioMixer;
     public string parameterName;
 
+    public float lowDB = -50.0f;
+
     private void Awake()
     {
         Slider slider = GetComponent<Slider>();
 
         audioMixer.GetFloat(parameterName, out float dB);
-        slider.value = ReLerp(-80, 0, slider.minValue, slider.maxValue, dB);
+        slider.value = ReLerp(lowDB, 0, slider.minValue, slider.maxValue, dB);
 
         slider.onValueChanged.AddListener(fac =>
         {
-            audioMixer.SetFloat(parameterName, ReLerp(slider.minValue, slider.maxValue, -80, 0, fac));
+            if (Mathf.Approximately(fac, slider.minValue))
+            {
+                audioMixer.SetFloat(parameterName, -80);
+            }
+            else
+            {
+                audioMixer.SetFloat(parameterName, ReLerp(slider.minValue, slider.maxValue, lowDB, 0, fac));
+            }
         });
     }
 
