40bcc8d99482617fb6f79ad376e812c43c933be1
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-10 03:09:50 +0000

    Hide mouse while in-game

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -6,11 +6,22 @@ public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
 
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.visible = true;
+    }
+
     private void Update()
     {
         if (Input.GetButtonDown("Cancel"))
         {
             pauseMenu.SetActive(!pauseMenu.activeSelf);
+            Cursor.visible = pauseMenu.activeSelf;
         }
     }
 }
