a2ec93b878c49644843b9753554324a0543b1705
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-01 23:13:18 +0100

    Simple SharedData class for global shared data

---
+++ Assets/Scripts/SharedData/ResetSharedData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ResetSharedData : MonoBehaviour
+{
+    [SerializeField]
+    private FilterType filterType = FilterType.Blacklist;
+
+    public enum FilterType
+    {
+        Whitelist,
+        Blacklist
+    }
+
+    [SerializeField]
+    private Shared[] filterList;
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        IEnumerable<Shared> toReset;
+        if (filterType == FilterType.Whitelist)
+        {
+            toReset = filterList;
+        }
+        else
+        {
+            toReset = Object.FindObjectsOfType<Shared>().Except(filterList);
+        }
+
+        foreach (Shared shared in toReset)
+        {
+            shared.Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/SharedData/Shared.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public abstract class Shared : ScriptableObject
+{
+    public abstract void Reset();
+}
+
+public class Shared<T> : Shared
+{
+    [SerializeField]
+    private T value;
+
+    public T Value
+    {
+        get => value;
+        set => this.value = value;
+    }
+
+    public static implicit operator T(Shared<T> shared)
+    {
+        return shared.value;
+    }
+
+    [SerializeField]
+    private T resetValue;
+
+    public override void Reset()
+    {
+        value = resetValue;
+    }
+}
+
+[CreateAssetMenu]
+public class SharedBool : Shared<bool> { }
