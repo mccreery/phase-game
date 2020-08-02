8c3eb783a63aa33d982b6ef240b1c747dc963858
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-02 01:13:10 +0100

    Remove music singleton, create audio source when needed

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip music;
+
+    [SerializeField]
+    private AudioMixerGroup output;
+
+    private void Start()
+    {
+        GameObject gameObject = GameObject.Find(nameof(BackgroundMusic));
+        AudioSource audioSource;
+
+        if (gameObject == null)
+        {
+            gameObject = new GameObject(nameof(BackgroundMusic));
+            DontDestroyOnLoad(gameObject);
+
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.loop = true;
+            audioSource.outputAudioMixerGroup = output;
+        }
+        else
+        {
+            audioSource = gameObject.GetComponent<AudioSource>();
+        }
+
+        if (audioSource.clip != music || !audioSource.isPlaying)
+        {
+            audioSource.Stop();
+            audioSource.clip = music;
+            audioSource.Play();
+        }
+    }
+}
