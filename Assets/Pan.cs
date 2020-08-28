885ce62294ed1997e9550952944e60de0d2bb3c7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-28 22:14:54 +0100

    Add background to main menu

---
+++ Assets/Pan.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Pan : MonoBehaviour
+{
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+    public float speed = 1;
+
+    private void Start()
+    {
+        transform.position = startPosition;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+
+        if (transform.position.Equals(endPosition))
+        {
+            transform.position = startPosition;
+        }
+    }
+}
